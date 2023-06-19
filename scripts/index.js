ccb43ad9101f05c3e1fa17fc67e0a5698205a168
[Tiffania] <ielenatiffania@gmail.com>
2023-06-19 09:51:44 -0400

    added button play in  js

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,62 @@
-// Players
-const player1 = "Player One";
-const player2 = "Player Two";
-
-let currentPlayer = player1;
-let originalPlayer = player1; // Original player who chose the question
-
+//? Functions
 function switchPlayer() {
     if (currentPlayer === player1) {
-        return player2;
+        currentPlayer = player2;
     } else {
-        return player1;
+        currentPlayer = player1;
+    }
+    playersTurnDisplay.textContent = currentPlayer; // Update the text content of the playersTurnDisplay (h2 element)
+    return currentPlayer;
+}
+
+// Function to display the alert
+function showPlayersTurn(currentPlayer) {
+    // We want for the alert to be displayed in all the pages but the index.html
+    // const urlIndexHTML = window.location.href --> http://127.0.0.1:5501/
+    // when at this URL we won't show the alert
+
+    let currentPageURL = window.location.href;
+    if (currentPageURL === "http://127.0.0.1:5501/") {
+        return;
+    } else {
+        alert(`It's ${currentPlayer}'s turn!`);
+        // To disable the buttons:
+        const userButtons = document.getElementsByClassName("user-buttons"); // first we select all the buttons and wrap them in one variable
+        userButtons.disabled = true;
     }
 }
 
-// Scores
+
+//? Players
+const player1 = "Player One";
+const player2 = "Player Two";
+const playersTurnDisplay = document.getElementById("players-turn-display");
+let currentPlayer = player1;
+let originalPlayer = player1; // Original player who chose the question
+
+//? Scores
 let scoreDisplay1 = document.getElementById("scoreP1");
 let scoreDisplay2 = document.getElementById("scoreP2");
 let scorePlayer1 = 0;
 let scorePlayer2 = 0;
 
 //? These are our containers to store our data:
-const tableCells = document.querySelectorAll(".clickable-item");
-const modal = document.querySelector("#modal");
+const playButton = document.getElementById("button-play");
 const guessButton = document.querySelector("#guess-btn");
 const passButton = document.querySelector("#pass-btn");
 const nextRoundButton = document.querySelectorAll("#next-round-btn")
 let inputAnswer = document.querySelector("#input-answer");
 let inputBet = document.getElementById("input-bet");
+//--------------------------------------------------------------------
+const tableCells = document.querySelectorAll(".clickable-item");
+const modal = document.querySelector("#modal");
 let currentQuestion;
 let offSet;
 
-//? Imports
+//? Import
 // Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
 console.log({ placeholderQuestions });
-import { showPlayersTurn } from "./functions.js"
-
-
-window.addEventListener("load", () => showPlayersTurn(currentPlayer)); // notification for player's turn
 
 //? Questions
 // Questions are organized by categoy using the .filter() method 
@@ -76,6 +95,12 @@ const qmap = {
     final: questionFinal
 }
 
+
+playButton.addEventListener("click", function () {
+    window.location.href = "round-1.html";
+});
+window.addEventListener("load", () => showPlayersTurn(currentPlayer)); // notification for player's turn
+
 //? CELLS AND CORRISPONDING OF QUESTIONS
 // We add an event listener to each cell of the table
 tableCells.forEach(c => c.addEventListener("click", function () { //every time we click on c this function is going to run
@@ -89,6 +114,8 @@ tableCells.forEach(c => c.addEventListener("click", function () { //every time w
 
     //Let's turn the offSet into a number calling parseInt, so we can use it to see how far we to go into the array
     const offSetToNumber = parseInt(offSet) - 1; //Since the index counting starts from 0 in order to get the right number we have to subtract 1
+
+    c.disabled = true;
 
     //This will create a pop up that will display the category and worth of the cell that we click on
     alert(`Category ${category}, Worth $${offSet + '00'}`)
@@ -144,7 +171,5 @@ guessButton.addEventListener("click", function () {
 passButton.addEventListener("click", function () {
     currentPlayer = switchPlayer();
     alert(`${currentPlayer}, it's your turn to answer.`);
-    console.log(currentPlayer);
-    modal.style.display = "none";
 });
 
